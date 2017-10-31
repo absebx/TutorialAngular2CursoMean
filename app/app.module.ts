ebd541f5b702c91b700d6e470c9e6f9ab3fa2a64
asder707 <abn.sebick@gmail.com>
2017-10-30 22:26:41 -0300

    servicio y obtener los elementos de la api

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { HttpModule, JsonpModule} from '@angular/http';
+
 import { AppComponent }  from './app.component';
 import { FavoritosListComponent} from './components/favoritos-list.component';
-import { FormsModule} from '@angular/forms';
  
 @NgModule({
-  imports:      [ BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
   //aquí van los componentes que vamos a ocupar en nuestra app
   declarations: [
-     AppComponent,
-     FavoritosListComponent
-    ],
-  bootstrap:    [ AppComponent ]
+    AppComponent,
+    FavoritosListComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
--- app/components/favoritos-list.component.ts
+++ app/components/favoritos-list.component.ts
@@ -1,16 +1,43 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+
+import {FavoritoService} from '../services/favorito.service';
+import {Favorito} from '../models/favorito';
 
 //decorador
 @Component({
   selector: 'favoritos-list',
-  templateUrl: 'app/views/favoritos-list.html'
+  templateUrl: 'app/views/favoritos-list.html',
+  providers: [FavoritoService]
 })
 
-export class FavoritosListComponent{
+export class FavoritosListComponent implements OnInit{
   public title: string;
+  public favoritos: Favorito[];
+  public errorMessage;
 
-  constructor(){
+  //EL Constructor asigna valores
+  constructor(private _favoritoService: FavoritoService){
     this.title = 'Listado de marcadores';
+  }
+
+  //esto se ejecuta al principio del controlador y puede tener cualquier logica
+  ngOnInit(){
+    console.log("favoritos cargados");
+    this._favoritoService.getFavoritos()
+      .subscribe(result => {
+        console.log(result);
+        this.favoritos = result.favoritos;
+
+        if(!this.favoritos){
+          alert("error en el servidor");
+        }
+      }, error => {
+        this.errorMessage = <any>error;
+        if(this.errorMessage != null){
+          console.log(this.errorMessage);
+          alert("Error en la petición");
+        }
+      });
   }
   
 }
