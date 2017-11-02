c06afd1e203d2f885f363ac433ebc130f508ac5c
asder707 <abn.sebick@gmail.com>
2017-11-02 16:37:55 -0300

    Fromulario para crear favorito

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,10 +2,12 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {FavoritosListComponent} from './components/favoritos-list.component';
 import {FavoritoDetailComponent} from './components/favorito-detail.component';
+import {FavoritoAddComponent} from './components/favorito-add.component';
 //array con definicion de todas las rutas
 const appRoutes: Routes = [
   {path: '', component: FavoritosListComponent},
   {path: 'marcador/:id', component: FavoritoDetailComponent},
+  {path: 'add-marcador', component: FavoritoAddComponent},
   {path: '**', component: FavoritosListComponent} //esta siempre tiene que estar al final
 ];
 
---
+++ app/components/favorito-add.component.ts
@@ -0,0 +1,30 @@
+// Importar Component desde el núcleo de Angular
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+import {FavoritoService} from '../services/favorito.service';
+import {Favorito} from '../models/favorito';
+
+// Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+@Component({
+   selector: 'favorito-add',
+   templateUrl: 'app/views/favorito-add.html' 
+})
+
+// Clase del componente donde irán los datos y funcionalidades
+export class FavoritoAddComponent implements OnInit{ 
+  public titleSection: string;
+  public favorito: Favorito;
+  constructor(){
+    this.titleSection = "Crear favorito";
+  }
+
+  ngOnInit(){
+    this.favorito = new Favorito("","","");
+    console.log(this.favorito);
+  }
+
+  public onSubmit(){
+    console.log(this.favorito);
+  }
+}
